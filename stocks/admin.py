f847ae3652ee213cbc9416315e220675c57ef836
Sherry Lee <sherrymta520@gmail.com>
2025-08-01 16:58:09 +0800

    Modify models.py

--- stocks/admin.py
+++ stocks/admin.py
@@ -4,6 +4,12 @@ from .models import InvestmentRecord
 
 @admin.register(InvestmentRecord)
 class InvestmentRecordAdmin(admin.ModelAdmin):
-    list_display = ('transaction_type', 'stock_name', 'buy_date', 'sell_date', 'net_profit', 'annual_return_rate')
+    list_display = ('transaction_type', 'stock_name', 'buy_date', 'sell_date', 'net_profit', 'annual_return_rate_percent')
     list_filter = ('transaction_type', 'stock_name')
     search_fields = ('stock_name',)
+
+    def annual_return_rate_percent(self, obj):
+        if obj.annual_return_rate is None:
+            return "-"
+        return f"{obj.annual_return_rate * 100:.2f}%"
+    annual_return_rate_percent.short_description = '年化報酬率'
--- stocks/models.py
+++ stocks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class InvestmentRecord(models.Model):
-    transaction_id = models.IntegerField(max_length=100, unique=True, verbose_name="交易ID", default='default_id')
+    transaction_id = models.IntegerField(unique=True, verbose_name="交易ID", default='default_id')
     TRANSACTION_CHOICES = [
         ('BUY', '買'),
         ('SELL', '賣'),
@@ -15,13 +15,13 @@ class InvestmentRecord(models.Model):
     buy_date = models.DateField(verbose_name="買進日期")
     buy_price = models.FloatField(verbose_name="買進價")
     quantity = models.IntegerField(verbose_name="張數")
-    amount = models.FloatField(verbose_name="金額")
+    buy_amount = models.IntegerField(verbose_name="買進金額")
     fee_rate = models.FloatField(verbose_name="手續費率")
     fee_amount = models.FloatField(verbose_name="手續費金額")
-    total_cost = models.FloatField(verbose_name="總成本")
+    total_cost = models.IntegerField(verbose_name="總成本")
     sell_date = models.DateField(null=True, blank=True, verbose_name="賣出日期")
-    sell_price = models.FloatField(null=True, blank=True, verbose_name="賣出價")
-    net_profit = models.FloatField(null=True, blank=True, verbose_name="淨利")
+    sell_amount = models.IntegerField(null=True, blank=True, verbose_name="賣出總額")
+    net_profit = models.IntegerField(null=True, blank=True, verbose_name="淨利")
     profit_rate = models.FloatField(null=True, blank=True, verbose_name="獲利率")
     annual_return_rate = models.FloatField(null=True, blank=True, verbose_name="年化報酬率")
     holding_days = models.IntegerField(null=True, blank=True, verbose_name="持有天數")
